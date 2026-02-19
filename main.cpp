28f4432fe166d65e7f3d0f17629294b3d73d4877
SMP <parakalasaimukund@gmail.com>
2026-02-19 15:30:46 +0530

    base files

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "math.h"
+
+int main(){
+    std::cout << "sum is " << add(4,5) << std::endl;
+    std::cout << "prod is " << mul(4,5) << std::endl;
+    return 0;
+}
---
+++ math.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "math.h"
+
+int add(int a , int b){
+    return a + b + 5;
+}
+
+int mul(int a , int b){
+    return a*b;
+}
